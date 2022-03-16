082dd9791045e35a4e6f8015f7c0dd98c005be72
Nikita_Koryushkin <nickkorya@gmail.com>
2022-03-16 22:46:53 +0300

    Cycles JS

--- src/Cycles.js
+++ src/Cycles.js
@@ -5,7 +5,7 @@
 */
 export function rangeSum(start, end) {
   let sum = 0
-  for (i = start ;i < end + 1 ;i++){
+  for (let i = start ;i < end + 1 ;i++){
     if (i % 2 === 0) sum += i
   }
   return sum
